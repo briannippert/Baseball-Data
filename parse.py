12bd023e819070f3ce90781ccba4db8412c209c2
Andrew <andrew.stencavage@engility.com>
2018-10-18 07:43:30 -0400

    added parsePlay

--- parse.py
+++ parse.py
@@ -4,7 +4,24 @@ strikePlays = ['C','K','M','O','Q','S','T']
 ballPlays = ['B','I','P','V']
 
 def parsePlay(play,prevPlay):
-    pass
+    pitchPlay = play[4]
+    fieldPlay = play[5]
+
+    strikes = 0
+    balls = 0
+    puts = 0
+    for p in pitchPlay:
+        if(p in strikePlays):
+            strike +=1
+            if (strike == 3):
+                outs += 1
+        if(p in ballPlays):
+            balls+=1
+            if(balls == 4):
+                pass#advance runners
+    
+    
+
 
 with open('./2017eve/2017BOS.EVA') as fp:
     gameId = None
