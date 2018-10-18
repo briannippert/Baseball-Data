5e5d5c036dcff3c347116c95375e104c3b021b57
Brian Nippert <briannippert@gmail.com>
2018-10-18 14:51:06 -0400

    Changes from today's working session

--- atBat.py
+++ atBat.py
@@ -2,8 +2,14 @@ import json
 import pitch
 
 class atBat:
-    def __init__(self,pitches,inning):
-            self.pitches = pitches
+    def __init__(self,pitches,inning, first,second,third,outs,rundiff, outcome):
+        self.pitches = pitches
+        self.inning = inning
+        self.first = first
+        self.second = second
+        self.third = third
+        self.outs = outs
+        self.rundiff = rundiff
     def toJSON(self):
         data = {}
         data['pitches'] = self.pitches
--- parse.py
+++ parse.py
@@ -3,44 +3,70 @@ import os
 
 strikePlays = ['C','K','M','O','Q','S','T']
 ballPlays = ['B','I','P','V']
+activePlay = ['H','X','Y']
 
-def advanceRunners(batter,firstBase,secondBase,thirdBase, count):
-    runsScored = 0
-    curCount = 0
-    while count <= curCount:
-        if thirdBase == true:
-            thirdBase = false
-            runsScored += 1
-        if secondBase == true:
-            secondBase = false
-            thirdBase = true
-        if firstBase == true:
-            firstBase = false
-            secondBase = true
-        if batter == true:
-            batter = false
-            firstBase = true
-        curCount += 1
-    return rundiff
-
-def parseFieldPlay(fieldPlay):
+def parseFieldPlay(fieldPlay,first,second,third,scoreDiff,outs,isBottom):
     slashPos = fieldPlay.find('/')
     periodPos = fieldPlay.find('.')
     batterPlay = fieldPlay
     runnerPlay = None
-    if(slashPos > -1):
-        # print(fieldPlay)
-        batterPlay=batterPlay[:slashPos]
-    if(periodPos > -1):
-        runnerPlay = fieldPlay[periodPos+1:]
-    # print(batterPlay)
-    if(runnerPlay):
-        print(runnerPlay)
+    batterOut = False
 
-def parsePlay(play,prevPlay):
+    if(slashPos > -1):
+        batterPlay=batterPlay[:slashPos]
+
+    if(periodPos != -1):
+        runnerPlay = fieldPlay[periodPos+1:]
+        runPlays = runnerPlay.split(';')
+
+        starts = []
+        ends = []
+        for p in runPlays:
+            if(p[1]=='X'):
+                #players out
+                if(p[0]==1):
+                    first=False
+                elif(p[0]==2):
+                    second=False
+                elif(p[0]==3):
+                    third=False
+                elif(p[0]=='B'):
+                    batterOut = True
+                outs+=1
+            else:#if(p[1]=='-')
+                start = p[0]
+                end = p[2]
+                starts.append(start)
+                ends.append(end)
+
+        for start in starts:
+            if(start == 1):
+                first=False
+            elif(start==2):
+                second=False
+            elif(start==3):
+                third=False 
+        for end in ends:
+            if(end == 1):
+                first=True
+            elif(end==2):
+                second=True
+            elif(end==3):
+                third=True
+            else:#HOME
+                if(isBottom == 1):
+                    scoreDiff += 1
+                else:
+                    scoreDiff -= 1  
+
+    return {'first':first,'second':second,'third':third,'scoreDiff':scoreDiff,'outs':outs}               
+
+def parseAtBat(play,prevAtBat):
+    inning = play[0]
+    isBottom = play[1] #top = 0, bottom = 1
     pitchPlay = play[4]
     fieldPlay = play[5]
-
+    pitches = []
     strikes = 0
     balls = 0
     
@@ -49,13 +75,19 @@ def parsePlay(play,prevPlay):
     first = False
     second = False 
     third = False
-    if(prevPlay != None):
-        prevPlay = prevPlay.getCurrentState()
-        outs = prevPlay.outs
-        scoreDiff = prevPlay.scoreDiff
-        first = prevPlay.first
-        second = prevPlay.second
-        third = prevPlay.third
+
+   
+    if(prevAtBat != None):
+        prevState = prevAtBat.getCurrentState()
+        outs = prevState.outs
+        scoreDiff = prevState.scoreDiff
+        first = prevState.first
+        second = prevState.second
+        third = prevState.third
+        if('.' in pitchPlay):
+            strikes = prevState.strikes
+            balls = prevState.balls
+            pitches = prevAtBat.pitches
     if(outs == 3):
         outs = 0
         first = False
@@ -67,14 +99,19 @@ def parsePlay(play,prevPlay):
             strikes +=1
             if (strikes == 3):
                 outs += 1
-        if(p in ballPlays):
+        elif(p in ballPlays):
             balls+=1
-            if(balls == 4):
-                pass#advance runners
-    # if('.' in fieldPlay):
-    #     print(pitchPlay,'           ',fieldPlay)
-    parseFieldPlay(fieldPlay)
-    # print('Strikes: {} Balls: {} Outs: {} Pitches: {}'.format(strikes,balls,outs,pitchPlay))
+        elif(p not in activePlay): continue
+        newPitch = pitch(ball, strike, out, scoreDiff, p, first, second, third, JSON=None)
+        pitches.append(newPitch)
+    retVals = parseFieldPlay(fieldPlay,first,second,third,scoreDiff,outs,isBottom)
+    first = retVals["first"]
+    second = retVals["second"]
+    third = retVals["third"]
+    scoreDiff = retVals["scoreDiff"]
+    outs = retVals["outs"]
+    print('Strikes: {} Balls: {} Outs: {} ScoreDiff: {} Pitches: {}'.format(strikes,balls,outs,scoreDiff,pitchPlay))
+    # retBat = atBat()
     
 def readFile(f):
     with open(f) as fp:
@@ -83,17 +120,18 @@ def readFile(f):
         for line in fp:
             lineDetailed = line.strip().split(',')
             if(lineDetailed[0]=='id'):
-                
                 if(gameId != None):
                     games[gameId] = game
             if(lineDetailed[0]=='play'):
                 if(lineDetailed[6]=='NP' and lineDetailed[5] == ''): continue #ignore non-injury subs
-                prevPlay = None
+                prevAtBat = None
                 if (len(game)>0):
-                    prevPlay = game[-1]
-                atBat = parsePlay(lineDetailed[1:], None)
+                    prevAtBat = game[-1]
+                if(lineDetailed[5].find('.') != -1):
+                    game.pop()
+                atBat = parseAtBat(lineDetailed[1:], prevAtBat)
                 game.append(atBat)
-#os.path.join('app', 'subdir', 'dir', 'filename.foo')
+
 filePath = os.path.join('.',"2017eve","2017BOS.EVA")
 readFile(filePath)
 
