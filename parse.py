56548553dafd7357e364ec1d92d4452114e77c13
Andrew <andrew.stencavage@engility.com>
2018-10-18 07:27:22 -0400

    Added strike and ball plays

--- parse.py
+++ parse.py
@@ -1,17 +1,21 @@
 games = {}
 
+strikePlays = ['C','K','M','O','Q','S','T']
+ballPlays = ['B','I','P','V']
+
+def parsePlay(play,prevPlay):
+    pass
+
 with open('./2017eve/2017BOS.EVA') as fp:
     gameId = None
     game = None
     for line in fp:
-        # print(line.strip())
         lineDetailed = line.strip().split(',')
-        # print(lineDetailed)
         if(lineDetailed[0]=='id'):
-            # print(lineDetailed[1])
             
             if(gameId != None):
                 games[gameId] = game
         if(lineDetailed[0]=='play'):
-            if(lineDetailed[6]=='NP' and lineDetailed[5] == ''): pass #ignore non-injury subs
-            print(lineDetailed[1:])
+            if(lineDetailed[6]=='NP' and lineDetailed[5] == ''): continue #ignore non-injury subs
+            parsePlay(lineDetailed[1:])
+
