13656f6aac8452a86fdcb1bf06185d4a047e5635
Brian Nippert <briannippert@gmail.com>
2018-10-22 09:12:33 -0400

    worked on project

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,18 +1,22 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include "json.hpp"
 
+using nlohmann::json;
 using namespace std;
 
 string readFile(string);
+void parseJSON(string);
 
 int main()
 {
     string name;
     cout << "Enter File Name: ";
-    cin >> name;
-    string data = readFile(name);
-    cout << data;
+  //  cin >> name;
+    string data = "hello";//readFile(name);
+    // cout << data;
+    parseJSON(data);
     return 0;
 }
 
@@ -34,4 +38,11 @@ string readFile(string path)
         return "Unable to open file";
     }
     return data;
+}
+
+void parseJSON(string data)
+{
+    //   cout << data << "\n";
+   string jsonTest = "{ \"name\":\"John\", \"age\":30, \"car\":null }";
+    cout << json::parse(jsonTest);
 }
