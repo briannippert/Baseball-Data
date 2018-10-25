081f8c4edd698c6330a4c3311dfe6d053639f399
Andrew <andrew.stencavage@engility.com>
2018-10-25 14:27:58 -0400

    changed parse to output pitch array

--- mango.py
+++ mango.py
@@ -3,7 +3,7 @@ import pymongo
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
 mydb = myclient["Baseball-Data"]
-myCol = mydb["BaseBall-Data"]
+myCol = mydb["pitches"]
 
 # myquery = {"atBats.inning":"116"}
 
@@ -12,29 +12,5 @@ myCol = mydb["BaseBall-Data"]
 # for x in mydoc:
 #   print(x)
 
-data = myCol.aggregate([
-    {'$match': {'atBats.inning':'116'}},
-    {'$unwind': {'path':'$atBats.pitches'}}
-    
-    # {'$match':}
-])
-
-# data = myCol.aggregate([
-#     {'$match': {
-#         '$and': 
-#             [{'atBats.pitches.strike':2},
-#             {'atBats.inning':'116'}]
-#     }}
-#     {'$project': {
-#         'atBats': {'$filter': {
-#             'input': '$atBats',
-#             'as': 'atBat',
-#             'cond': {'$eq':
-#                 ['$$atBats.inning']
-#             }
-#         }}
-
-#     }}
-# ])
-for x in data:
-    print(x)
+x = myCol.find()
+print(x[0])
