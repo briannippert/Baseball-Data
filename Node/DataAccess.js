95fea504c187fed3d8dc7d4b37f3bc81491e2fd6
Stencavage <Andrew.Stencavage@ENGILITY.COM>
2018-10-26 16:03:41 -0400

    added some console logs to dataAccess for testing

--- Node/DataAccess.js
+++ Node/DataAccess.js
@@ -4,7 +4,7 @@ const test = require('assert');
 const url = 'mongodb://localhost:27017';
 const dbName = 'BaseBall-Data';
 
-
+games = []
 function getStats(ball,strike,out,scoreDiff,first,second,third,inning) {
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
@@ -21,14 +21,25 @@ function getStats(ball,strike,out,scoreDiff,first,second,third,inning) {
         }  
         dbo.collection("pitches").find(query).toArray(function(err, result) {
           if (err) throw err;
-          console.log(result)
-          console.log(result.length);
+        //   console.log(result)
+        //   console.log(result.length);
           filteredTotal = 0
           for (i in result){
               if(result[i].winningTeam == true){
                 filteredTotal++;
               }
+              let gameId = result[i]["id"]
+              if(gameId in games){
+                  console.log(result[i]["id"])
+              }
+              if(!gameId.endsWith("0")){
+                  console.log(gameId)
+                  console.log(result[i])
+              }
+              games.push(gameId)
+              
           }
+          console.log(games.length)
           console.log(filteredTotal)
           db.close();
         });
