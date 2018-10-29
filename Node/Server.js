7a104a5c0a1cf4149fca3a6bf6da5d28e8d93f70
Brian Nippert <briannippert@gmail.com>
2018-10-29 09:02:51 -0400

    made changes

--- Node/Server.js
+++ Node/Server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/Calculate', function (req, res) {
+    res.send(`This Doesn't work Yet!`)
+  });
 
 app.use(express.static('public'));
 app.get('/', (req, res) => res.send('Hello World!'));
