e81f01aed300f2d6f3cc2c93ae7ee6ee12b75db0
Brian Nippert <briannippert@gmail.com>
2018-10-29 09:35:18 -0400

    updated server

--- Node/Server.js
+++ Node/Server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const DataAccess = require('./DataAccess');
 const app = express();
 const port = 3000;
 
 app.get('/Calculate', function (req, res) {
-    res.send(`This Doesn't work Yet!`)
-  });
+  DataAccess.getStats(1, 1, 1, 1, 1, 1, 1, 01);
+  res.send(`This Doesn't work Yet!`)
+});
 
 app.use(express.static('public'));
 app.get('/', (req, res) => res.send('Hello World!'));
