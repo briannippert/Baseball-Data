fc8e80325c8b20c49d3373de762bd98ddbb3c223
Brian Nippert <briannippert@gmail.com>
2018-10-29 10:50:38 -0400

    Worked on server

--- Node/Server.js
+++ Node/Server.js
@@ -5,12 +5,23 @@ const port = 3000;
 
 app.get('/Calculate', function (req, res) {
   var url = require('url');
-  var url_parts = url.parse(request.url, true);
+  var url_parts = url.parse(req.url, true);
   var query = url_parts.query;
-  dataAccess.getStats(query.balls, 1, 1, 1, 1, 1, 1, "01");
-  res.send(`This Doesn't work Yet!`);
+  var inning = query.topBottom + "" + query.inning;
+  if (query.first == null) {
+    query.first = false;
+  }
+  if (query.second == null) {
+    query.second = false;
+  }
+  if (query.third == null) {
+    query.third = false;
+  }
+  console.log("First Base: " + query.first);
+  console.log(inning);
+  var nums = dataAccess.getStats(query.balls, query.strikes, query.outs, query.scoreDiff, query.first, query.second, query.third, inning);
+  res.send(nums);
 });
 
 app.use(express.static('public'));
-app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
