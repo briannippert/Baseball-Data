70c446b68a49223b72e1cee461647422b7c5fb1b
Andrew <andrew.stencavage@engility.com>
2018-10-29 11:02:23 -0400

    getStats allows strings

--- Node/DataAccess.js
+++ Node/DataAccess.js
@@ -5,113 +5,114 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'BaseBall-Data';
 
 games = [];
-module.exports = {
-    getStats: function (ball, strike, out, scoreDiff, first, second, third, inning) {
-        MongoClient.connect(url, function (err, db) {
-            if (err) throw err;
-            var dbo = db.db("Baseball-Data");
-            var query = {
-                '$and': [{
-                        'ball': ball
-                    },
-                    {
-                        'strike': strike
-                    },
-                    {
-                        'out': out
-                    },
-                    {
-                        'scoreDiff': scoreDiff
-                    },
-                    {
-                        'first': first
-                    },
-                    {
-                        'second': second
-                    },
-                    {
-                        'third': third
-                    },
-                    {
-                        'inning': inning
-                    }
-                ]
-            };
-            dbo.collection("pitches").find(query).toArray(function (err, result) {
-                if (err) throw err;
-                filteredTotal = 0;
-                for (i in result) {
-                    //   console.log(result[i])
-                    if (result[i].winningTeam == true) {
-                        filteredTotal++;
-                    }
-                    let gameId = result[i]["id"];
-                    if (gameId in games) {
-                        console.log(result[i]["id"]);
-                    }
-                    if (!gameId.endsWith("0")) {
-                        console.log(gameId);
-                        console.log(result[i]);
-                    }
-                    games.push(gameId);
+// module.exports = {
+//     getStats: function (ball, strike, out, scoreDiff, first, second, third, inning) {
+//         MongoClient.connect(url, function (err, db) {
+//             if (err) throw err;
+//             var dbo = db.db("Baseball-Data");
+//             var query = {
+//                 '$and': [{
+//                         'ball': ball
+//                     },
+//                     {
+//                         'strike': strike
+//                     },
+//                     {
+//                         'out': out
+//                     },
+//                     {
+//                         'scoreDiff': scoreDiff
+//                     },
+//                     {
+//                         'first': first
+//                     },
+//                     {
+//                         'second': second
+//                     },
+//                     {
+//                         'third': third
+//                     },
+//                     {
+//                         'inning': inning
+//                     }
+//                 ]
+//             };
+//             dbo.collection("pitches").find(query).toArray(function (err, result) {
+//                 if (err) throw err;
+//                 filteredTotal = 0;
+//                 for (i in result) {
+//                     //   console.log(result[i])
+//                     if (result[i].winningTeam == true) {
+//                         filteredTotal++;
+//                     }
+//                     let gameId = result[i]["id"];
+//                     if (gameId in games) {
+//                         console.log(result[i]["id"]);
+//                     }
+//                     if (!gameId.endsWith("0")) {
+//                         console.log(gameId);
+//                         console.log(result[i]);
+//                     }
+//                     games.push(gameId);
 
-                }
-                console.log(games.length);
-                console.log(filteredTotal);
-                db.close();
-            });
-        });
-    }
-}
-
-
-// function getStats(ball,strike,out,scoreDiff,first,second,third,inning) {
-//     MongoClient.connect(url, function(err, db) {
-//         if (err) throw err;
-//         var dbo = db.db("Baseball-Data");
-//         var query = {'$and': 
-//             [{'ball':ball},
-//                 {'strike':strike},
-//                 {'out':out},
-//                 {'scoreDiff':scoreDiff},
-//                 {'first':first},
-//                 {'second':second},
-//                 {'third':third},
-//                 {'inning':inning}]
-//         };
-//         dbo.collection("pitches").find(query).toArray(function(err, result) {
-//           if (err) throw err;
-//           filteredTotal = 0;
-//           for (i in result){
-//             //   console.log(result[i])
-//               if(result[i].winningTeam == true){
-//                 filteredTotal++;
-//               }
-//               let gameId = result[i]["id"];
-//               if(gameId in games){
-//                   console.log(result[i]["id"]);
-//               }
-//               if(!gameId.endsWith("0")){
-//                   console.log(gameId);
-//                   console.log(result[i]);
-//               }
-//               games.push(gameId);
-
-//           }
-//           console.log(games.length);
-//           console.log(filteredTotal);
-//           db.close();
+//                 }
+//                 console.log(games.length);
+//                 console.log(filteredTotal);
+//                 db.close();
+//             });
 //         });
-//     });
+//     }
 // }
 
-// getStats(
-//     ball=0,
-//     strike=0,
-//     out=0,
-//     scoreDiff=0,
-//     first=false,
-//     second=false,
-//     third=false,
-//     inning='01'
-// );
+
+function getStats(ball,strike,out,scoreDiff,first,second,third,inning) {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("Baseball-Data");
+        var query = {'$and': 
+            [{'ball':parseInt(ball)},
+                {'strike':parseInt(strike)},
+                {'out':parseInt(out)},
+                {'scoreDiff':parseInt(scoreDiff)},
+                {'first':first},
+                {'second':second},
+                {'third':third},
+                {'inning':inning}]
+        };
+        dbo.collection("pitches").find(query).toArray(function(err, result) {
+          if (err) throw err;
+          filteredTotal = 0;
+          for (i in result){
+            //   console.log(result[i])
+              if(result[i].winningTeam == true){
+                filteredTotal++;
+              }
+              let gameId = result[i]["id"];
+              if(gameId in games){
+                  console.log(result[i]["id"]);
+              }
+              if(!gameId.endsWith("0")){
+                  console.log(gameId);
+                  console.log(result[i]);
+              }
+              games.push(gameId);
+
+          }
+          console.log(games.length);
+          console.log(filteredTotal);
+          console.log(filteredTotal / games.length)
+          db.close();
+        });
+    });
+}
+
+getStats(
+    ball=0,
+    strike=0,
+    out=0,
+    scoreDiff=0,
+    first=false,
+    second=false,
+    third=false,
+    inning='01'
+);
